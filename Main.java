c9f7a7765df542276603d83775d31a28ebf3f7d2
ss <>
2020-07-31 18:02:51 +0900

    HoT: Initial revision

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+public class Main{
+     public static void main(String[] args){
+        
+        Random random = new Random();
+        int ran = random.nextInt(2);
+        int heads=0,tail=0;
+        System.out.println("Tossing a coin...");
+        for(int i=1;i<=3;i++)
+        {
+            if(ran==0){
+                System.out.println("Round "+i+": Heads");
+                heads++;
+            }
+            else {
+                System.out.println("Round "+i+": tail");
+                tail++;
+            }
+            ran = random.nextInt(2);
+        }
+        System.out.println("Heads:"+heads+"Tails"+tail); 
+        
+        }
+    }
