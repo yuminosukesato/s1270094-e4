8457b7d4a11bdcf0bbabc201a916734ea6ee8bb1
ss <>
2020-07-31 18:06:26 +0900

    User name added

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 import java.util.Random;
+import java.util.Scanner;
 public class Main{
      public static void main(String[] args){
+        System.out.println("Who are you?");
+        Scanner sc1 = new Scanner(System.in);
+        String line1 = sc1.nextLine();
+        System.out.println("Hello,"+line1+"!");
         
         Random random = new Random();
         int ran = random.nextInt(2);
